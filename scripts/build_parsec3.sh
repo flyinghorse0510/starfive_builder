002c315dadaae37cf9d06d0c534fa3ba40f8446f
flyinghorse0510 <mhy@flyinghorse.top>
2023-11-08 15:16:23 +0800

    add initail support for parsec3_builder docker

--- scripts/build_parsec3.sh
+++ scripts/build_parsec3.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Copy parsec3 source
-echo "Copying parsec3 source into the container"
+echo "copying parsec3 source into the container"
 mkdir -p /home/starfive/
 cp -R /parsec3 /home/starfive/parsec3
 
 
 # Enter parsec3 source directory
-cd /home/starfive/parsec3
+cd /home/starfive/parsec3 || exit
+echo "source env.sh"
 # source env.sh
 . env.sh
 
@@ -15,9 +16,36 @@ cd /home/starfive/parsec3
 if [ "${1}" != "all" ]; then
     export BENCHMARKS="$*"
 fi
+echo "set build target ==> [ ${BENCHMARKS} ]"
 
-./bin/build
+# Split target string as list
+IFS=" "
+read -ra benchmarkList <<< "${BENCHMARKS}"
 
+echo "build target"
+for benchmark in "${benchmarkList[@]}"
+do
+    echo "building ${benchmark}"
+    if [ "${benchmark}" = "raytrace" ]; then
+        # raytrace doesn't support static link, use standalone build configuration
+        cp config/gcc-raytrace.bldconf config/gcc.bldconf
+    else
+        cp config/gcc-common.bldconf config/gcc.bldconf
+    fi
+    export BENCHMARKS="${benchmark}"
+    # Redirect stdout to clear the output
+    ./bin/build > "${benchmark}.build_log"
+done
+
+# Create output directory
+echo "create output directory"
 mkdir -p /output/images
+mkdir -p /output/logs
 
+# Copy built binary files
+echo "copy binary files"
 rsync -a images/ /output/images
+
+# Copy build logs
+echo "copy build logs"
+cp *.build_log /output/logs/
---
+++ scripts/get_parsec3_src.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Clone the source
+mkdir -p src
+git clone https://github.com/flyinghorse0510/parsec-starfive.git --depth 1 --recursive --shallow-submodules src/parsec3
+
+# pull the lfs (input data) of parsec3
+cd src/parsec3 || exit
+git lfs pull
+
